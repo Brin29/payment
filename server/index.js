c176a51ac1ab6f2653c4643ede240b50fe449ce8
Brin29 <breinerstivenparracortes50@gmail.com>
2024-10-07 16:44:29 -0500

    cambios

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import stripe from 'stripe'
+import Stripe from 'stripe'
 import cors from 'cors'
 
 const app = express()
+
+const stripe = new Stripe("sk_test_51OW2cUDdNCdWc3LAuYyNETm7NE9fdy36yaDeV6dDivWju8qyfk0C5YQVEbiSWNouA7tcwdeVhpC72f8R5wiMkTre001p0MovE8")
 
 app.use(cors({ origin: "http://localhost:5173" }))
 app.use(express.json)
